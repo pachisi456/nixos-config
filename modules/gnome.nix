1cd0a9bc168b86df9cd216f7761c1110f76a37bf
Luca Dommes <luca.dommes@devterm.de>
2022-02-09 19:07:25 +0100

    feat(gnome): Enable extensions and add random-wallpaper

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -6,11 +6,16 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.core-utilities.enable = false;
 
+  nixpkgs.config.firefox.enableGnomeExtensions = true;
+  services.gnome.chrome-gnome-shell.enable = true;
+
   environment.systemPackages = with pkgs; [
     gnome.gedit
     gnome.gnome-panel
     gnome.gnome-screenshot
     gnome.gnome-tweaks
     gnome.nautilus
+
+    gnomeExtensions.random-wallpaper
   ];
 }
