b4f56473b0dff82455d71926c97379ab22f26001
Luca Dommes <luca.dommes@devterm.de>
2022-02-16 22:43:45 +0100

    feat(zsh): Add xrandr shortcuts useful for i3-only setup

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -22,6 +22,10 @@
       config = "sudo vim /etc/nixos/configuration.nix";
       update = "sudo nixos-rebuild switch";
 
+      # Set up 2 or 3 monitors. Done automatically with gnome. Useful for i3-only setup.
+      xrandr-2 = "xrandr --output eDP-1 --off && xrandr --output DP-1-2 --auto && xrandr --output DP-1-1 --auto --left-of DP-1-2";
+      xrandr-3 = "xrandr --output DP-1-2 --auto --left-of eDP-1 && xrandr --output DP-1-1 --auto --left-of DP-1-2";
+
       # Other.
       mux = "tmuxinator";
     };
