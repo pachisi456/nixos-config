7f2f84bf42c83f1c5dddce003a3a97ef67d49b97
Luca Dommes <luca.dommes@devterm.de>
2022-05-03 19:43:53 -0500

    feat(zsh): Add upgrade alias

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -20,6 +20,7 @@
     shellAliases = {
       # NixOS.
       update = "sudo nixos-rebuild switch";
+      upgrade = "sudo nix-channel --update && update";
 
       # Set up 2 or 3 monitors. Done automatically with gnome. Useful for i3-only setup.
       xrandr-2 = "xrandr --output eDP-1 --off && xrandr --output DP-1-2 --auto && xrandr --output DP-1-1 --auto --left-of DP-1-2";
